ed0aacadc3e9d06a6d22c47bac9271192dc1128e
Pekka Tuukkanen <petuukka@gmail.com>
2020-10-05 20:36:03 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   index.php                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ptuukkan <ptuukkan@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/05 18:47:06 by ptuukkan          #+#    #+#             */
+/*   Updated: 2020/10/05 18:47:06 by ptuukkan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	jepaje <br>
+	<?=$_SERVER["REQUEST_URI"]?>
+</body>
+</html>
