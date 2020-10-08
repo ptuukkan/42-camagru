0d6f2f8aaa91980f9394d8356448a2bda568bda0
Pekka Tuukkanen <petuukka@gmail.com>
2020-10-08 20:19:43 +0300

    emptied index

--- index.php
+++ index.php
@@ -11,28 +11,3 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<script type="module" src="./components/paragraph-group.js"></script>
-	<script type="module" src="./components/my-paragraph.js"></script>
-	<script type="module" src="./components/header-menu.js"></script>
-	<script type="module" src="./components/top-header.js"></script>
-	<script type="module" src="./components/user-panel.js"></script>
-	<script type="module" src="./components/login-modal.js"></script>
-	<title>Document</title>
-</head>
-<body>
-	<top-header>
-		<header-menu slot="header-menu"></header-menu>
-		<user-panel slot="user-panel"></user-panel>
-	</top-header>
-
-
-</body>
-</html>
