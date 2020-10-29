399b2e41554c6dd5460c66b2fd94d4e959bb833e
Pekka Tuukkanen <petuukka@gmail.com>
2020-10-29 21:05:40 +0200

    Edit page layout

--- public/js/edit.js
+++ public/js/edit.js
@@ -3,46 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   edit.js                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ptuukkan <ptuukkan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ptuukkan <ptuukkan@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/27 21:09:45 by ptuukkan          #+#    #+#             */
-/*   Updated: 2020/10/27 22:18:37 by ptuukkan         ###   ########.fr       */
+/*   Updated: 2020/10/29 20:53:01 by ptuukkan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 const clearPhoto = (canvas, photo) => {
 	const context = canvas.getContext("2d");
-	context.fillStyle = "#EEE";
+	context.fillStyle = "#AAA";
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	const data = canvas.toDataURL('image/png');
 	photo.setAttribute('src', data);
 }
 
-const runWebcam = () => {
-	const width = document.querySelector("#webcam-container").clientWidth;
-	console.log(width);
-	let height = null;
+const setupWebCam = () => {
 	const video = document.querySelector("#video");
 	const canvas = document.querySelector("#canvas");
-	const photo = document.querySelector("#photo");
 	const takePhotoButton = document.querySelector("#takephoto");
+	const photo = document.querySelector("#photo");
 	let streaming = false;
 
-	console.log("getting user media");
-	navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-		.then((stream) => {
-			video.srcObject = stream;
-			video.play();
-		})
-		.catch((error) => {
-			console.log("error:" + error);
-		});
-		
 	console.log("adding canplay event listener");
 	video.addEventListener("canplay", (event) => {
 		console.log("canplay listener triggered");
-
 		if (!streaming) {
+			console.log("not streaming");
 			height = video.videoHeight / (video.videoWidth / width);
 			if (isNaN(height)) {
 				height = width / (4 / 3);
@@ -53,8 +40,7 @@ const runWebcam = () => {
 			canvas.setAttribute('height', height);
 			streaming = true;
 		}
-		
-	}, false);
+	});
 
 	console.log("adding take photo event listener");
 	takePhotoButton.addEventListener("click", (event) => {
@@ -70,9 +56,70 @@ const runWebcam = () => {
 		}
 		event.preventDefault();
 	})
-	
-	clearPhoto(canvas, photo);
 }
 
-runWebcam();
+const webCamMode = () => {
+	console.log("webcam mode");
+	const video = document.querySelector("#video");
+	const webCamToggle = document.querySelector("#webcamtoggle");
 
+	console.log("getting user media");
+	navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+		.then((stream) => {
+			video.srcObject = stream;
+			video.play();
+		})
+		.catch((error) => {
+			webCamToggle.checked = false;
+			webCamToggle.disabled = true;
+			return uploadMode();
+		});
+
+	//clearPhoto(canvas, photo);
+}
+
+const setupUpload = () => {
+	const uploadInput = document.querySelector("#upload");
+	const uploadButton = document.querySelector("#uploadphoto");
+	const uploadIcon = document.querySelector("#uploadicon");
+	const saveButton = document.querySelector("#savephoto");
+
+	uploadButton.addEventListener("click", (event) => {
+		document.querySelector("#upload").click();
+	});
+
+	uploadInput.addEventListener("change", (event) => {
+		if (event.target.files && event.target.files[0]) {
+			photo.setAttribute('src', URL.createObjectURL(event.target.files[0]));
+			uploadIcon.style.display = "none";
+			if (saveButton.classList.contains("disabled")) {
+				saveButton.classList.remove("disabled");
+			}
+		}
+	})
+}
+
+const uploadMode = () => {
+	console.log("upload mode");
+	const uploadIcon = document.querySelector("#uploadicon");
+	const takePhotoButton = document.querySelector("#takephoto");
+	const video = document.querySelector("#video");
+	const uploadButton = document.querySelector("#uploadphoto");
+
+	uploadButton.style.display = "";
+	video.style.display = "none";
+	uploadIcon.style.display = "";
+	takePhotoButton.style.display = "none";
+}
+
+document.querySelector("#webcamtoggle").addEventListener("click", (event) => {
+	if (event.target.checked) {
+		webCamMode();
+	} else {
+		uploadMode();
+	}
+})
+
+setupUpload();
+setupWebCam();
+webCamMode();
--- views/edit.php
+++ views/edit.php
@@ -14,23 +14,50 @@
 
 <div class="ui grid">
 	<div class="ui fluid card ten wide column centered">
-		<div class="image" id="webcam-container">
+    <div class="content">
+			<div class="right floated extra content">
+        <div class="ui toggle checkbox">
+          <input type="checkbox" name="webcam" id="webcamtoggle" checked>
+          <label>Use WebCam</label>
+        </div>
+      </div>
+		</div>
+		<div class="image" id="webcam-container" style="text-align: center">
 			<video id="video">Video stream not available.</video>
+      <i class="upload icon huge" style="margin: 50px; display: none" id="uploadicon"></i>
+      <img id="photo">
 		</div>
 		<div class="content" style="text-align: center">
+			<input type="file" hidden id="upload" />
+			<button class="ui button primary" id="uploadphoto" style="display: none">Upload photo</button>
 			<button class="ui button primary" id="takephoto">Take photo</button>
+      <button class="ui button primary disabled" id="savephoto">Save photo</button>
 		</div>
 	</div>
+	<div class="four wide column centered">
+	<div class="ui segment">
+	<div class="ui divided items">
+  <div class="item">
+    <div class="image">
+      <img src="/images/wireframe/image.png">
+    </div>
+  </div>
+  <div class="item">
+    <div class="image">
+      <img src="/images/wireframe/image.png">
+    </div>
+  </div>
+  <div class="item">
+    <div class="image">
+      <img src="/images/wireframe/image.png">
+    </div>
+  </div>
 </div>
+	</div>
 
-<div class="camera">
-    
-    
+	</div>
 </div>
 <canvas id="canvas" style="display: none">
-  </canvas>
-  <div class="output">
-    <img id="photo" alt="The screen capture will appear in this box.">
-  </div>
+</canvas>
 <script src=/public/js/edit.js></script>
 
